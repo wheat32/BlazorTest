fa81113c7df30109ba63c06f391e4f30fc7ace2e
wheat32 <nickpage3232@yahoo.com>
2022-05-11 17:07:15 -0400

    Showing code-behind, added comments, updated presentation

---
+++ BlazorTest/Pages/FetchData.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace BlazorTest.Pages
+{
+    public partial class FetchData : ComponentBase
+    {
+        [Inject]
+        private HttpClient Http { get; set; }
+
+        private WeatherForecast[]? forecasts;
+
+        protected override async Task OnInitializedAsync() => forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
+
+        public class WeatherForecast
+        {
+            public DateTime Date { get; set; }
+
+            public int TemperatureC { get; set; }
+
+            public string? Summary { get; set; }
+
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        }
+    }
+}
--- BlazorTest/Program.cs
+++ BlazorTest/Program.cs
@@ -2,10 +2,13 @@ using BlazorTest;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+//Top level statements that will be placed into a main method when compiled
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+//Initializing the HttpClient service and setting its base address to the host environment's base address
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
